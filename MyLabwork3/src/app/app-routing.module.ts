e5771c04c25081bf5a39d0cb7368d4c4a4fb4cc3
nelli.nurmilaukas <nelli.Nurmilaukas75@gmail.com>
2024-03-17 01:06:25 +0200

    Update routing and menu content

--- MyLabwork3/src/app/app-routing.module.ts
+++ MyLabwork3/src/app/app-routing.module.ts
@@ -5,6 +5,25 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  { path: 'add-song', loadChildren: () => import('./add-song/add-song.module').then(m => m.AddSongPageModule) 
+},
+  
+  {
+    path: 'logout',
+    loadChildren: () => import('./logout/logout.module').then( m => m.LogoutPageModule)
+  },
+  {
+    path: 'songs',
+    loadChildren: () => import('./songs/songs.module').then( m => m.SongsPageModule)
+  },
+  {
+    path: 'update-song',
+    loadChildren: () => import('./update-song/update-song.module').then( m => m.UpdateSongPageModule)
+  },
+  {
+    path: 'song-list',
+    loadChildren: () => import('./song-list/song-list.module').then( m => m.SongListPageModule)
   }
 ];
 @NgModule({
